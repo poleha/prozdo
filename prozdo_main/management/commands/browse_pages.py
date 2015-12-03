936e39b12f597fdb6545b3af7305deb9b1c3ce4f
Alex Poleha <alex@poleha.ru>
2015-12-03 15:34:08 +0300

    Added browse_pages management command

---
+++ prozdo_main/management/commands/browse_pages.py
@@ -0,0 +1,38 @@
+from django.core.management.base import BaseCommand
+from prozdo_main import models
+from super_model import models as super_models
+from urllib.request import urlopen
+from django.core.urlresolvers import reverse
+import time
+from django.utils import timezone
+from django.core.mail import mail_admins
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        count = 0
+
+        urls = tuple()
+        urls += (reverse('blog-list'),)
+        urls += (reverse('component-list'),)
+        urls += (reverse('drug-list'),)
+        urls += (reverse('cosmetics-list'),)
+        urls += (reverse('main-page'),)
+        urls += ('/sitemap.xml',)
+
+        post_urls = (post.get_absolute_url() for post in models.Post.objects.filter(status=super_models.POST_STATUS_PUBLISHED)
+                     if post.last_modified > (timezone.now() - timezone.timedelta(seconds=60 * 35)))
+
+        urls += tuple(post_urls)
+        errors = []
+
+        for url in urls:
+            count += 1
+            absolute_url = 'http://prozdo.ru{0}'.format(url)
+            res = urlopen(absolute_url)
+            if not res.code == 200:
+                errors.append('{0}-{1}'.format(url, res.code))
+            time.sleep(1)
+
+        if len(errors) > 0:
+            mail_admins('Errors during crawling', '\n'.join(errors))
