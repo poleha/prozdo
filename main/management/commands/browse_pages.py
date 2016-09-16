5f37a2aa1e6bbaa50b0503a5c6951b7b981b2a6f
poleha <pass1191>
2016-09-16 09:45:33 +0300

    browse_pages management command improved

--- main/management/commands/browse_pages.py
+++ main/management/commands/browse_pages.py
@@ -10,6 +10,7 @@ from cache.decorators import construct_cached_view_key
 from django.core.cache import cache
 from main.views import PostDetail
 from random import shuffle
+from main.models import Post
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
@@ -29,15 +30,24 @@ class Command(BaseCommand):
                             default=False,
                             help='Show visited pages')
 
-        parser.add_argument('--no_sleep',
-                            action='store_true',
-                            dest='no_sleep',
-                            default=False,
+        parser.add_argument('--sleep_time',
+                            dest='sleep_time',
+                            type=float,
+                            default=0.1,
                             help='Sleep after each visit')
+
+        parser.add_argument('--exec_time',
+                            dest='exec_time',
+                            type=int,
+                            default=60,
+                            help='Max execution time')
 
 
     def handle(self, *args, **options):
+        start_time = time.time()
         count = 0
+        sleep_time = options['sleep_time']
+        exec_time = options['exec_time']
 
         urls = tuple()
         urls += (reverse('blog-list'),)
@@ -59,8 +69,8 @@ class Command(BaseCommand):
                     res = requests.get(absolute_url, headers=headers)
                     if not res.status_code == 200:
                         errors.append('{0}-{1}'.format(url, res.status_code))
-                    if not options['no_sleep']:
-                        time.sleep(0.5)
+                    if sleep_time:
+                        time.sleep(sleep_time)
                     if options['show']:
                         print('Visited url {}, {} of {}. Response code: {}'.format(absolute_url, count, urls_len,
                                                                                res.status_code))
@@ -70,13 +80,22 @@ class Command(BaseCommand):
         posts = list(models.Post.objects.filter(status=super_models.POST_STATUS_PUBLISHED))
         shuffle(posts)
         for post in posts:
+            cur_time = time.time()
+            if cur_time - start_time > exec_time:
+                break
             if not (post.is_blog or post.is_drug or post.is_component or post.is_cosmetics):
                 continue
             count += 1
             absolute_url = url = '{}{}'.format(settings.SITE_URL, post.get_absolute_url())
-            key = construct_cached_view_key(PostDetail.get, url=absolute_url)
-            mobile_key = key.replace('flavour_None', 'flavour_mobile')
-            full_key = key.replace('flavour_None', 'flavour_full')
+            alias = getattr(post, 'alias', None)
+            if alias:
+                original_key = construct_cached_view_key(PostDetail.get, url=absolute_url, model_class=Post, kwarg='alias',
+                                            alias=post.alias)
+            else:
+                original_key = construct_cached_view_key(PostDetail.get, url=absolute_url, model_class=Post, kwarg='pk',
+                                                pk=post.pk)
+            mobile_key = original_key.replace('flavour_None', 'flavour_mobile')
+            full_key = original_key.replace('flavour_None', 'flavour_full')
             for key in (full_key, mobile_key):
                 resp = cache.get(key)
                 if resp is None or options['full']:
@@ -88,8 +107,8 @@ class Command(BaseCommand):
                         if res.status_code != 200:
                             errors.append('{0}-{1}'.format(url, res.status_code))
 
-                        if not options['no_sleep']:
-                            time.sleep(0.5)
+                        if sleep_time:
+                            time.sleep(sleep_time)
                         if options['show']:
                             print(
                                 'Visited url {}, {} of {}. Response code: {}'.format(absolute_url, count, urls_len,
--- try.py
+++ try.py
@@ -13,42 +13,35 @@ if __name__ == "__main__":
 from main import models
 from super_model import models as super_models
 import requests
-from django.core.urlresolvers import reverse
 import time
 from django.conf import settings
 from django.core.mail import mail_admins
 from cache.decorators import construct_cached_view_key
 from django.core.cache import cache
 from main.views import PostDetail
-
+from main.models import Post
 
 def handle(**options):
     count = 0
-
-    urls = tuple()
-    urls += (reverse('blog-list'),)
-    urls += (reverse('component-list'),)
-    urls += (reverse('drug-list'),)
-    urls += (reverse('cosmetics-list'),)
-    urls += (reverse('main-page'),)
-    urls += ('/sitemap.xml',)
-
-
     errors = []
-
-    urls_len = len(urls) + models.Post.objects.filter(status=super_models.POST_STATUS_PUBLISHED).count()
-
-
-
-    for post in models.Post.objects.filter(status=super_models.POST_STATUS_PUBLISHED, title='Редуксин'):
-        if not(post.is_blog or post.is_drug or post.is_component or post.is_cosmetics):
+    urls_len = models.Post.objects.filter(status=super_models.POST_STATUS_PUBLISHED).count()
+    posts = list(models.Post.objects.filter(status=super_models.POST_STATUS_PUBLISHED))
+    #shuffle(posts)
+    for post in posts:
+        if not (post.is_blog or post.is_drug or post.is_component or post.is_cosmetics):
             continue
         count += 1
-        absolute_url = url='{}{}'.format(settings.SITE_URL, post.get_absolute_url())
-        key = construct_cached_view_key(PostDetail.get, url=absolute_url)
-        mobile_key = key.replace('flavour_None', 'flavour_mobile')
-        full_key = key.replace('flavour_None', 'flavour_full')
-        for key in (full_key, mobile_key):
+        absolute_url = url = '{}{}'.format(settings.SITE_URL, post.get_absolute_url())
+        alias = getattr(post, 'alias', None)
+        if alias:
+            original_key = construct_cached_view_key(PostDetail.get, url=absolute_url, model_class=Post, kwarg='alias',
+                                                     alias=post.alias)
+        else:
+            original_key = construct_cached_view_key(PostDetail.get, url=absolute_url, model_class=Post, kwarg='pk',
+                                                     pk=post.pk)
+        mobile_key = original_key.replace('flavour_None', 'flavour_mobile')
+        full_key = original_key.replace('flavour_None', 'flavour_full')
+        for key in (full_key, mobile_key, original_key):
             resp = cache.get(key)
             if resp is None or options['full']:
                 headers = {}
@@ -59,19 +52,20 @@ def handle(**options):
                     if res.status_code != 200:
                         errors.append('{0}-{1}'.format(url, res.status_code))
 
-                    if not options['no_sleep']:
-                        time.sleep(0.5)
                     if options['show']:
-                        print('Visited url {}, {} of {}. Response code: {}'.format(absolute_url, count, urls_len, res.status_code))
+                        print(
+                            'Visited url {}, {} of {}. Response code: {}'.format(absolute_url, count, urls_len,
+                                                                                 res.status_code))
                 except:
                     errors.append('{0}-{1}'.format(url, 'EXCEPTION'))
 
-    if len(errors) > 0:
-        mail_admins('Errors during crawling', '\n'.join(errors))
 
 options = {
-    'show': False,
-    'no_sleep': False,
-    'full': False
+    'show': True,
+    'full': False,
+    'sleep_time': 0.1,
+
+
 }
 handle(**options)
+
